b866c98cd4e0c79d0d4dc39e903458e0ad6aec30
kimdongil <skypentum@gmail.com>
2016-07-11 23:51:43 +0900

    build

---
+++ task7-react_build/build/broswerify/src/index.js
@@ -0,0 +1,7 @@
+var ReactDOM = require('react-dom');
+var React = require('react');
+
+var App = require('./components/app');
+ 
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
---
+++ task7-react_build/build/gulp_build/src/js/app.js
@@ -0,0 +1,9 @@
+var app = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h1>Hello React Skeleton</h1>
+      </div>
+    )
+  }
+});
